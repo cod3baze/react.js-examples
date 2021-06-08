f0273f75a483d238a27ddb267d0b499e8cd293c1
eliasall3x <elliasallex@gmail.com>
2021-06-08 12:28:15 -0300

    feat(hooks): useState type

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface IUserData {
+  name: string;
+  login: string;
+  avatar_url: string;
+}
 
 const App: React.FC = () => {
-  return <div />;
+  const [user, setUser] = useState<IUserData>();
+
+  async function loadData() {
+    const response = await fetch("https://api.github.com/users/eliasallex");
+    const data = await response.json();
+
+    setUser(data);
+  }
+
+  return (
+    <div>
+      <strong>{user?.name}</strong>
+    </div>
+  );
 };
 
 export default App;
